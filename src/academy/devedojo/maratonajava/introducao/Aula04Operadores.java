26fabb4071268e38cbc9c169ea2ea8db5ae55e91
Daniel Ramos <financdaniel@gmail.com>
2025-02-11 17:44:00 -0300

    Operadores

--- src/academy/devedojo/maratonajava/introducao/Aula04Operadores.java
+++ src/academy/devedojo/maratonajava/introducao/Aula04Operadores.java
@@ -22,6 +22,35 @@ public class Aula04Operadores {
         System.out.println("isDezIgualQueVinte "+isDezIgualQueVinte);
         System.out.println("isDezDiferenteQueDez "+isDezDiferenteQueDez);
 
-        //
+        // && (AND) || (or) !
+        int idade = 35;
+        float salario = 3500F;
+        boolean isDentroDaLeiMaiorQueTrinta = idade > 30 && salario > 4612;
+        boolean isDentroDaLeiMenorQueTrinta = idade < 30 && salario >= 3381;
+
+        System.out.println("isDentroDaLeiMaiorQueTrinta "+isDentroDaLeiMaiorQueTrinta);
+        System.out.println("isDentroDaLeiMenorQueTrinta "+isDentroDaLeiMenorQueTrinta);
+
+        double valorTotalContaCorrente = 200;
+        double valorTotalContaPoupanca = 10000;
+        float valorPlaystation = 5000F;
+        boolean isPlaystationCincoCompravel = valorTotalContaCorrente > valorPlaystation || valorTotalContaPoupanca > valorPlaystation;
+        System.out.println("isPlaystationCincoCompravel "+isPlaystationCincoCompravel);
+
+        // = += -= *= /= %=
+        double bonus = 1800;
+        bonus += 1000;
+        bonus -= 1000;
+        bonus *= 2;
+        bonus /= 2;
+        bonus %= 2;
+        System.out.println(bonus);
+
+        // ++ --
+        int contador = 0;
+        contador += 1; // contador = contador + 1;
+        contador++;
+        contador--;
+        System.out.println(contador);
     }
 }
