7ec8065a005b8024f49ccc356d3ffcf59b66fe51
Daniel Ramos <financdaniel@gmail.com>
2025-02-13 09:12:29 -0300

    Estuturas condicionais finalizada 7/7

---
+++ src/academy/devedojo/maratonajava/introducao/Aula05EstruturasCondicionais03.java
@@ -0,0 +1,18 @@
+package academy.devedojo.maratonajava.introducao;
+
+public class Aula05EstruturasCondicionais03 {
+    public static void main(String[] args) {
+        // Doar se salario > 5000
+        double salario = 6000;
+        // 1º maneira
+        // String mensagemDoar = "Eu vou doar 500 pro DevDojo";
+        // String mensagemNaoDoar = "Ainda não tenho condições, mas vou ter!";
+        // (condicao) ? verdadeiro : falso
+        // String resultado = salario > 5000 ? mensagemDoar : mensagemNaoDoar;
+        // System.out.println(resultado);
+
+        // 2º maneira
+        String resultado = salario > 5000 ? "Eu vou doar 500 pro DevDojo" : "Ainda não tenho condições, mas vou ter!";
+        System.out.println(resultado);
+    }
+}
